81cffc4313450491d8a81a6a7a81fdbbeb894914
Yinkar <hkrabs@yandex.com>
2022-04-12 16:24:50 +0300

    Clone repo

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/google/uuid v1.3.0
 )
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"log"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/go-git/go-git/v5"
 )
 
 type input struct {
@@ -35,13 +39,27 @@ func newScan(c *gin.Context) {
 	var newInput input
 	var newOutput output
 
+	// Get URL
 	if err := c.BindJSON(&newInput); err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	scanId := uuid.New()
 
 	newOutput.ScanId = scanId.String()
 
+	// Clone repo to /tmp
+	clonePath := filepath.Join("/tmp/src", newOutput.ScanId)
+
+	_, err := git.PlainClone(clonePath, false, &git.CloneOptions{
+		URL:	newInput.Url,
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Return Scan ID output
 	c.IndentedJSON(http.StatusCreated, newOutput)
 }
