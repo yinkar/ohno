254fb1ffb7675449dfdab644a5dec872fe255a60
Yinkar <hkrabs@yandex.com>
2022-04-12 16:04:06 +0300

    Request and response

--- main.go
+++ main.go
@@ -5,12 +5,22 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+type input struct {
+	Url	string `json:"url"`
+}
+
+type output struct {
+	ScanId string `json:"scan_id"`
+}
 
 func main() {
 	router := gin.Default()
 
 	router.GET("ping", ping)
+	router.POST("newscan", newScan)
 
 	router.Run("localhost:8080")
 
@@ -19,4 +29,19 @@ func main() {
 
 func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, "pong")
+}
+
+func newScan(c *gin.Context) {
+	var newInput input
+	var newOutput output
+
+	if err := c.BindJSON(&newInput); err != nil {
+		return
+	}
+
+	scanId := uuid.New()
+
+	newOutput.ScanId = scanId.String()
+
+	c.IndentedJSON(http.StatusCreated, newOutput)
 }
